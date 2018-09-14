2679cee1c16408ddea2ce31206dfb339714e1f50
cegal <camilo.garcia.larotta@gmail.com>
2018-09-14 11:30:44 -0400

    Implement sequentialMatrixMultiplication()
    Also add relevant tests

---
+++ A1/src/main/java/ca/mcgill/ecse420/a1/MatrixMultiplication.java
@@ -0,0 +1,90 @@
+package ca.mcgill.ecse420.a1;
+
+import java.util.InputMismatchException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class MatrixMultiplication {
+
+	private static final int NUMBER_THREADS = 1;
+	private static final int MATRIX_SIZE = 2000;
+
+	public static void main(String[] args) {
+
+		// Generate two random matrices, same size
+		double[][] a = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+		double[][] b = generateRandomMatrix(MATRIX_SIZE, MATRIX_SIZE);
+		sequentialMultiplyMatrix(a, b);
+		parallelMultiplyMatrix(a, b);
+	}
+
+	/**
+	 * Returns the result of a sequential matrix multiplication
+	 * The two matrices are randomly generated
+	 * @param a is the first matrix
+	 * @param b is the second matrix
+	 * @return the result of the multiplication
+	 * */
+	public static double[][] sequentialMultiplyMatrix(double[][] a, double[][] b) {
+		if (a[1].length != b.length) {
+			throw new InputMismatchException("Invalid matrix sizes to perform multiplication.");
+		}
+
+		final int ROWS_A = a.length;
+		final int COLS_A = a[0].length;
+		final int COLS_B = b[0].length;
+		double[][] C = new double[ROWS_A][COLS_B];
+
+		for (int r=0; r<ROWS_A; r++) {
+			for (int c=0; c<COLS_B; c++) {
+				for (int k=0; k<COLS_A; k++) {
+					C[r][c] += a[r][k] * b[k][c];
+				}
+			}
+		}
+
+		return C;
+	}
+
+	/**
+	 * Prints the 2D matrix in grid form
+	 * @param matrix the 2D matrix to print
+	 */
+	public static void printMatrix(double[][] matrix) {
+		for(int r=0; r<matrix.length; r++) {
+		   for(int c=0; c<matrix[r].length; c++)
+			   System.out.print(matrix[r][c] + " ");
+		   System.out.println();
+		}
+	}
+
+	/**
+	 * Returns the result of a concurrent matrix multiplication The two matrices are
+	 * randomly generated
+	 *
+	 * @param a is the first matrix
+	 * @param b is the second matrix
+	 * @return the result of the multiplication
+	 */
+        public static double[][] parallelMultiplyMatrix(double[][] a, double[][] b) {
+			//TODO
+			return null;
+	}
+
+        /**
+         * Populates a matrix of given size with randomly generated integers between 0-10.
+         * @param numRows number of rows
+         * @param numCols number of cols
+         * @return matrix
+         */
+        private static double[][] generateRandomMatrix (int numRows, int numCols) {
+             double matrix[][] = new double[numRows][numCols];
+        for (int row = 0 ; row < numRows ; row++ ) {
+            for (int col = 0 ; col < numCols ; col++ ) {
+                matrix[row][col] = (double) ((int) (Math.random() * 10.0));
+            }
+        }
+        return matrix;
+    }
+
+}
---
+++ A1/src/test/java/ca/mcgill/ecse420/a1/TestMatrixMultiplication.java
@@ -0,0 +1,83 @@
+package ca.mcgill.ecse420.a1;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TestMatrixMultiplication {
+
+    public class TestCase {
+        public double[][] a;
+        public double[][] b;
+        public double[][] expected;
+
+        public TestCase(double[][] a, double[][] b, double[][] expected) {
+            this.a = a;
+            this.b = b;
+            this.expected = expected;
+        }
+
+        public TestCase() {}
+    }
+
+
+
+    @Test
+    public void testSequentialMatrixMultiplication() {
+        TestCase[] testCases = new TestCase[3];
+        testCases[0] = new TestCase(
+            new double[][]{
+                { 1, 0, 0 },
+                { 0, 1, 0 },
+                { 0, 0, 1 }
+            },
+            new double[][]{
+                { 5, 5, 5 },
+                { 4, 4, 4 },
+                { 3, 3, 3 }
+            },
+            new double[][]{
+                { 5, 5, 5 },
+                { 4, 4, 4 },
+                { 3, 3, 3 }
+            }
+        );
+        testCases[1] = new TestCase(
+            new double[][]{
+                { 5, 5, 5 },
+                { 4, 4, 4 },
+                { 3, 3, 3 }
+            },
+            new double[][]{
+                { 1, 0, 0 },
+                { 0, 1, 0 },
+                { 0, 0, 1 }
+            },
+            new double[][]{
+                { 5, 5, 5 },
+                { 4, 4, 4 },
+                { 3, 3, 3 }
+            }
+        );
+        testCases[2] = new TestCase(
+            new double[][]{
+                { 1, 2, 3 },
+                { 4, 5, 6 },
+            },
+            new double[][]{
+                { 7, 8},
+                { 9, 10},
+                { 11, 12}
+            },
+            new double[][]{
+                { 58, 64 },
+                { 139, 154}
+            }
+        );
+        for (TestCase tc : testCases) {
+            assertArrayEquals(tc.expected, MatrixMultiplication.sequentialMultiplyMatrix(tc.a, tc.b));
+        }
+    }
+}
