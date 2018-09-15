4605a967013e9fa266d37976d1b0ba13933c546c
cegal <camilo.garcia.larotta@gmail.com>
2018-09-15 16:53:40 -0400

    Uncomment code

--- A1/src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
+++ A1/src/ca/mcgill/ecse420/a1/DiningPhilosophers.java
@@ -1,28 +1,22 @@
-// package ca.mcgill.ecse420.a1;
+package ca.mcgill.ecse420.a1;
 
-// import java.util.concurrent.ExecutorService;
-// import java.util.concurrent.Executors;
-// import java.util.concurrent.locks.ReentrantLock;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.locks.ReentrantLock;
 
-// public class DiningPhilosophers {
+public class DiningPhilosophers {
 
-// 	public static void main(String[] args) {
+	public static void main(String[] args) {
+		int numberOfPhilosophers = 5;
+        Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
+        Object[] chopsticks = new Object[numberOfPhilosophers];
+	}
 
-// 		int numberOfPhilosophers = 5;
-//                 Philosopher[] philosophers = new Philosopher[numberOfPhilosophers];
-//                 Object[] chopsticks = new Object[numberOfPhilosophers];
-// 	}
+	public static class Philosopher implements Runnable {
 
-// 	public static class Philosopher implements Runnable {
+		@Override
+		public void run() {
 
-
-
-// 		@Override
-// 		public void run() {
-
-// 		}
-
-
-// 	}
-
-// }
+		}
+	}
+}
