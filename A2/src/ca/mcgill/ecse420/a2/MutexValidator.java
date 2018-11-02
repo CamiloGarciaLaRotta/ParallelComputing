f5b56573c66c5e2b0cefc33e8dc1e918ec4936c7
cegal <camilo.garcia.larotta@gmail.com>
2018-11-01 20:49:20 -0400

    Flaky test

--- A2/src/ca/mcgill/ecse420/a2/MutexValidator.java
+++ A2/src/ca/mcgill/ecse420/a2/MutexValidator.java
@@ -3,47 +3,66 @@ package ca.mcgill.ecse420.a2;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MutexValidator {
 
-  static final int numThreads = 10;
-  static final int numIterations = 1000;
-  static List<Range> ranges = new ArrayList<Range>();;
+  static int counter;
+  static final int numThreads = 5;
+  static final int numIterations = 100;
+
   public static void main(String[] args) {
-    System.out.println("Filter lock implements mutual exclusivity: " + isValidMutex(FilterLock.class));
-    System.out.println("Bakery lock implements mutual exclusivity: " + isValidMutex(BakeryLock.class));
+    System.out.println("Filter lock implements mutual exclusivity: " + isValidMutex(FilterLock.class) + "\n");
+    System.out.println("Bakery lock implements mutual exclusivity: " + isValidMutex(BakeryLock.class) + "\n");
+    System.exit(0);
   }
 
   private static boolean isValidMutex(Object lockType) {
 
     ExecutorService executor = Executors.newFixedThreadPool(numThreads);
     ArrayList<Callable<Object>> tasks = new ArrayList<Callable<Object>>(numThreads);
+    counter = 0;
 
-    for (int i = 0; i < numThreads; i++) {
+    for (int i=0; i<numThreads; i++) {
       tasks.add(Executors.callable(new Task(lockType)));
     }
 
     try {
-      executor.invokeAll(tasks);
+      // executor.invokeAll(tasks);
+      List<Future<Object>> futures = executor.invokeAll(tasks);
+      for(Future<Object> future : futures){
+        System.out.print("Thread ended with status: ");
+        try{
+          future.get();
+        }
+        catch (CancellationException ce) {
+          System.out.println("cancelled");
+          continue;
+        } catch (ExecutionException ee) {
+          ee.printStackTrace();
+          System.out.println("exception");
+          continue;
+        } catch (InterruptedException ie) {
+          System.out.println("interrupted");
+          continue;
+        }
+        System.out.println("ok");
+      }
     } catch (InterruptedException e) {
       e.printStackTrace();
-      System.exit(0);
-    } finally {
       executor.shutdown();
+      System.exit(0);
     }
 
-    for (int i=0; i<ranges.size(); i++) {
-      for (int j=0; j<ranges.size(); j++) {
-        if (i != j && Range.overlap(ranges.get(i), ranges.get(j))) {
-          return false;
-        }
-      }
-    }
-    return true;
+    System.out.println("Final Counter value: " + counter);
+
+    return (counter == numIterations * numThreads);
   }
 
   public static class Task implements Runnable {
@@ -61,18 +80,19 @@ public class MutexValidator {
 
     @Override
     public void run() {
+      long t0, t1;
       for (int i=0; i<numIterations; i++) {
 
-        randomDelay(0, 5);
-        lockUnderTest.lock();
-        long t0 = System.currentTimeMillis();
-        randomDelay(0, 5);
-        lockUnderTest.unlock();
-        long t1 = System.currentTimeMillis();
-
+        randomDelay(0, 1);
+        // uncomment rangeLock and comment lockUnderTest to "test" the official java lock
+        // lockUnderTest.lock();
         rangeLock.lock();
-        ranges.add(new Range(t0, t1));
+        t0 = System.currentTimeMillis();
+        counter++;
+        randomDelay(0, 1);
+        // lockUnderTest.unlock();
         rangeLock.unlock();
+        t1 = System.currentTimeMillis();
       }
     }
 
